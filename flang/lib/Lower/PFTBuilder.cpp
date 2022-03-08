b5a25ff73cbb323e6d2a62a52363e9f132d9fa08
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2022-03-08 09:48:57 +0100

    Fix file name in header

--- flang/lib/Lower/PFTBuilder.cpp
+++ flang/lib/Lower/PFTBuilder.cpp
@@ -1,4 +1,4 @@
-//===-- PFTBuilder.cc -----------------------------------------------------===//
+//===-- PFTBuilder.cpp ----------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
